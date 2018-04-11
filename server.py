eba35e04d80ca1bd227a4b0d8d7bc597ef28c8f1
Orion Koepke <orion.koepke@ttu.edu>
2018-04-11 17:22:41 -0500

    Added the first crack at the server and index.html. It doesn't do stuff with the computer yet.

---
+++ server.py
@@ -0,0 +1,43 @@
+from flask import Flask, send_from_directory, jsonify, request
+import sh
+import re
+
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return send_from_directory("./public", "index.html")
+
+
+@app.route("/wifi")
+def getWifiNetworks():
+    answer = sh.ls(".").stdout.decode("utf-8")
+
+    answer = answer.strip()
+
+    answer = re.split("\\s+", answer)
+
+    print(answer)
+
+    return jsonify(answer)
+
+
+@app.route("/submit", methods=["POST"])
+def submit():
+    print("POSTED")
+    return "DONE"
+
+class Server:
+
+    def __init__(self, app):
+        self.app = app
+
+    def start(self):
+        self.app.run()
+
+
+if __name__ == "__main__":
+    server = Server(app)
+    server.start()
