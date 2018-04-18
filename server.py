0bb4b0cbd5fd27bed4dc451c29800c4db2db4157
Orion Koepke <orion.koepke@ttu.edu>
2018-04-18 17:32:47 -0500

    Added code to try turning on wifi.

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 from flask import Flask, send_from_directory, jsonify, request
+from string import Template
 import sh
 import re
 
@@ -13,15 +14,14 @@ def index():
 
 @app.route("/wifi")
 def getWifiNetworks():
+
+    # Get the names of all the wifi networks available.
     answer = sh.grep(sh.iwlist("wlan0", "scan"),  "-Po", "(?<=ESSID:).*").stdout.decode("utf-8")
 
+    # Put the wifi networks in answer into a list without duplication.
     answer = answer.strip()
-
     answer = re.split("\\s+", answer)
-
     answer = list(set(answer))
-
-    print(answer)
 
     return jsonify(answer)
 
@@ -29,12 +29,32 @@ def getWifiNetworks():
 @app.route("/submit", methods=["POST"])
 def submit():
 
+    # Get the data passed to the route.
     data = request.get_json()
 
     print("str :: " + str(data))
     print("POSTED")
 
+    template = None
+
+    # Read in the wpa_supplicant.conf template file.
+    with open("./config_file/wpa_supplicant.conf", "r") as file:
+        template = Template(file.read())
+
+    # Put the values passed by the user into the wpa_supplicant template.
+    wifi_signin = template.substitute(wifi_ssid=data["wifinetwork"],
+                                      wifi_password='"' + data["wifipassword"] + '"')
+
+    with open("/etc/wpa_supplicant/wpa_supplicant.conf", "a") as file:
+        file.write(wifi_signin)
+
+    sh.systemctl("stop", "dnsmasq").stdout
+    sh.systemctl("stop", "hostapd").stdout
+
+    sh.wpa_cli( "-i", "wlan0", "reconfigure").stdout
+
     return send_from_directory("./public", "index.html")
+
 
 class Server:
 
