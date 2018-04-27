137b88b302ad93e91413a864c4966a26a647fb1a
Orion Koepke <orion.koepke@ttu.edu>
2018-04-27 16:42:09 -0500

    Added some logging to the server file.

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 from flask import Flask, send_from_directory, jsonify, request
 from string import Template
+import logging
 import sh
 import re
 
@@ -33,17 +34,20 @@ def getWifiNetworks():
 @app.route("/submit", methods=["POST"])
 def submit():
 
+    logging.info("Began submitting data.")
+
     # Get the data passed to the route.
     data = request.get_json()
 
-    print("str :: " + str(data))
-    print("POSTED")
+    logging.info("Got data from the request sent.")
 
     global info
     info = {"username": data["username"],
             "password": data["password"],
             "deviceName": data["deviceName"],
             "webIP": data["webIP"]}
+
+    logging.info("Global variable server.info set.")
 
     template = None
 
@@ -58,18 +62,29 @@ def submit():
     with open("/etc/wpa_supplicant/wpa_supplicant.conf", "w") as file:
         file.write(wifi_signin)
 
+    logging.info("Set the /etc/wpa_supplicant/wpa_supplicant.conf file with name: %s, password: %s"
+                 % (data["wifiNetwork"], data["wifiPassword"]))
+
     with open("./config_files/dhcpcd.conf_blank", "r") as file:
         dhcpcd_conf = file.read()
 
     with open("/etc/dhcpcd.conf", "w") as file:
         file.write(dhcpcd_conf)
 
+    logging.info("Set /etc/dhcpcd.conf with blank dhcpcd.conf file.")
+
     sh.systemctl("restart", "dhcpcd").stdout
+
+    logging.info("Restarted dhcpcd with systemctl.")
 
     sh.systemctl("stop", "dnsmasq").stdout
     sh.systemctl("stop", "hostapd").stdout
 
+    logging.info("Stopped dnsmasq and hostapd with systemctl.")
+
     sh.wpa_cli( "-i", "wlan0", "reconfigure").stdout
+
+    logging.info("Turned on wifi with wpa_cli -i wlan0 reconfigure")
 
     stop()
 
@@ -77,21 +92,33 @@ def submit():
 
 
 def stop():
+
+    logging.info("Began stopping server.")
+
     func = request.environ.get('werkzeug.server.shutdown')
     if func is None:
         raise RuntimeError('Not running with the Werkzeug Server')
     func()
 
+    logging.info("Server stopped.")
+
     global running
     running = False
 
+    logging.info("Set server.running gloabl variable to False.")
+
 
 def start():
+
+    logging.info("Began starting the server.")
+
     with open("./config_files/dhcpcd.conf_filled", "r") as file:
         dhcpcd_conf = file.read()
 
     with open("/etc/dhcpcd.conf", "w") as file:
         file.write(dhcpcd_conf)
+
+    logging.info("Replaced the /etc/dhcpcd.conf file.")
 
 
     with open("./config_files/dnsmasq.conf", "r") as file:
@@ -100,6 +127,8 @@ def start():
     with open("/etc/dnsmasq.conf", "w") as file:
         file.write(dhcpcd_conf)
 
+    logging.info("Replaced the /etc/dnsmasq.conf file.")
+
 
     with open("./config_files/hostapd.conf", "r") as file:
         dhcpcd_conf = file.read()
@@ -107,15 +136,25 @@ def start():
     with open("/etc/hostapd/hostapd.conf", "w") as file:
         file.write(dhcpcd_conf)
 
+    logging.info("Replaced the /etc/hostapd/hostapd.conf file.")
+
     sh.systemctl("restart", "dhcpcd").stdout
+
+    logging.info("Restarted dhcpcd with systemctl.")
 
     sh.systemctl("start", "dnsmasq").stdout
     sh.systemctl("start", "hostapd").stdout
 
+    logging.info("Started dnsmasq and hostapd with systemctl.")
+
     global running
     running= True
 
+    logging.info("Set server.running gloabl variable to True.")
+
     app.run(host="0.0.0.0", port=5000)
+
+    logging.info("Started the app running on 0.0.0.0:5000.")
 
 
 if __name__ == "__main__":
